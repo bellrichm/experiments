de82d6a0eb08205ed40f47c968c52654c1e2f142
Rich Bell <bellrichm@gmail.com>
2024-03-02 17:37:02 -0500

    this is gold

--- sqlitequeries/test02.sql
+++ sqlitequeries/test02.sql
@@ -1,20 +1,16 @@
 /* For a 12 hour time period, 
  For each hour, get the average of the concentration */
 SELECT
-    (unixepoch() /3600 ) * 3600 + 3600 as epoch,
-    /*datetime((unixepoch() / 3600) * 3600 + 3600, 'unixepoch', 'localtime') as DTN,*/
-    dateTime,
-    datetime(dateTime, 'unixepoch', 'localtime') as DT,
+    /*dateTime,
+    datetime(dateTime, 'unixepoch', 'localtime') as DT,*/
     count(pm2_5) as countConcentration,
-    /*min(pm2_5) as minConcentration,*/
-    /*max(pm2_5) as maxConcentration,*/
     min(dateTime),
     max(dateTime),
-    datetime(min(dateTime), 'unixepoch', 'localtime') as minDT,
-    datetime(max(dateTime), 'unixepoch', 'localtime') as maxDT,
+    /*datetime(min(dateTime), 'unixepoch', 'localtime') as minDT,
+    datetime(max(dateTime), 'unixepoch', 'localtime') as maxDT,*/
     avg(pm2_5) as avgConcentration
 FROM archive
-WHERE dateTime >= (unixepoch() / 3600) * 3600 - 39600
+WHERE dateTime >= (unixepoch() / 3600) * 3600 - 39600  + 300 /* 300 is the archive interval */
     AND dateTime < (unixepoch() / 3600) * 3600 + 3600
 GROUP BY (dateTime - 300) / 3600 /* need to subtract the archive interval to get the correct begin and end range */
 HAVING avgConcentration IS NOT NULL
