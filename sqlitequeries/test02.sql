2b492cb731392cfc2d1172c957cf35915dcb9244
Rich Bell <bellrichm@gmail.com>
2024-03-02 17:27:21 -0500

    this results matches what weewx python code does

--- sqlitequeries/test02.sql
+++ sqlitequeries/test02.sql
@@ -1,13 +1,21 @@
-/* Need to double check the inclusivity of the timestamps */
-SELECT dateTime,
+/* For a 12 hour time period, 
+ For each hour, get the average of the concentration */
+SELECT
+    (unixepoch() /3600 ) * 3600 + 3600 as epoch,
+    /*datetime((unixepoch() / 3600) * 3600 + 3600, 'unixepoch', 'localtime') as DTN,*/
+    dateTime,
     datetime(dateTime, 'unixepoch', 'localtime') as DT,
     count(pm2_5) as countConcentration,
     /*min(pm2_5) as minConcentration,*/
     /*max(pm2_5) as maxConcentration,*/
+    min(dateTime),
+    max(dateTime),
+    datetime(min(dateTime), 'unixepoch', 'localtime') as minDT,
+    datetime(max(dateTime), 'unixepoch', 'localtime') as maxDT,
     avg(pm2_5) as avgConcentration
 FROM archive
-WHERE dateTime >= 1709366400
-    AND dateTime < 1709409600
-GROUP BY dateTime / 3600
+WHERE dateTime >= (unixepoch() / 3600) * 3600 - 39600
+    AND dateTime < (unixepoch() / 3600) * 3600 + 3600
+GROUP BY (dateTime - 300) / 3600 /* need to subtract the archive interval to get the correct begin and end range */
 HAVING avgConcentration IS NOT NULL
-ORDER BY dateTime DESC
+ORDER BY dateTime ASC
