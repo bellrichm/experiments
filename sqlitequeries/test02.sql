6caadb0d3aa4d6cfc08e7cd5ab07aea50d765ae5
Rich Bell <bellrichm@gmail.com>
2024-03-03 09:42:55 -0500

    ifnull example

--- sqlitequeries/test02.sql
+++ sqlitequeries/test02.sql
@@ -6,14 +6,12 @@ SELECT
     count(pm2_5) as countConcentration,
     min(dateTime),
     max(dateTime),
-    /*datetime(min(dateTime), 'unixepoch', 'localtime') as minDT,
-     datetime(max(dateTime), 'unixepoch', 'localtime') as maxDT,*/
-    avg(pm2_5) as avgConcentration
+    IFNULL(avg(pm2_5), 0) as avgConcentration
 FROM archive
 WHERE dateTime >= (unixepoch() / 3600) * 3600 - 39600 + 300
     /* 300 is the archive interval */
     AND dateTime < (unixepoch() / 3600) * 3600 + 3600
 GROUP BY (dateTime - 300) / 3600
     /* need to subtract the archive interval to get the correct begin and end range */
-HAVING avgConcentration IS NOT NULL
+    /*HAVING avgConcentration IS NOT NULL*/
 ORDER BY dateTime ASC
