df4a0b486aae2dc2449e2b55349e51005b46a257
Rich Bell <bellrichm@gmail.com>
2024-03-03 17:58:47 -0500

    example of getting count from the query

---
+++ sqlitequeries/test03.sql
@@ -0,0 +1,18 @@
+Select COUNT()
+FROM (
+        SELECT
+            /*dateTime,
+             datetime(dateTime, 'unixepoch', 'localtime') as DT,*/
+            count(pm2_5) as countConcentration,
+            min(dateTime),
+            max(dateTime),
+            IFNULL(avg(pm2_5), 0) as avgConcentration
+        FROM archive
+        WHERE dateTime >= (unixepoch() / 3600) * 3600 - 39600 + 300
+            /* 300 is the archive interval */
+            AND dateTime < (unixepoch() / 3600) * 3600 + 3600
+        GROUP BY (dateTime - 300) / 3600
+            /* need to subtract the archive interval to get the correct begin and end range */
+            /*HAVING avgConcentration IS NOT NULL*/
+        ORDER BY dateTime ASC
+    ) AS ROWCOUNT
